5fcb1192194b2c7e1d7b54f64df7f4a5fb22e824
souhinbou <souhinbounianng@gmail.com>
2022-11-30 23:39:23 +0000

    migration de la table demande et ajout du champ role dans users

--- database/migrations/2022_11_30_231542_add_role_field_to_users_table.php
+++ database/migrations/2022_11_30_231542_add_role_field_to_users_table.php
@@ -13,12 +13,8 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('demandes', function (Blueprint $table) {
-            $table->id();
-            $table->string('objet');
-            $table->boolean('status')->default(false);
-            $table->text('description');
-            $table->timestamps();
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('role')->nullable();
         });
     }
 
@@ -29,6 +25,8 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('demandes');
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('role');
+        });
     }
 };
